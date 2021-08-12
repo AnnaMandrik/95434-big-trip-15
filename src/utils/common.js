db82c4eace5bc19ca64847d55152fb93a05bf7e4
Анна <AnnaMandrik1984@yandx.ru>
2021-08-12 17:32:47 +0300

    Разделяй и властвуй

---
+++ src/utils/common.js
@@ -0,0 +1,17 @@
+// Функция из интернета по генерации случайного числа из диапазона
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+
+const getRandomArray = (arr) => {
+  const results = [];
+  results.push(arr.slice(0, Math.ceil(Math.random() * arr.length)));
+  return results;
+};
+
+export {getRandomInteger, getRandomArray};
--- src/view/route-form-create.js
+++ src/view/route-form-create.js
@@ -1,26 +1,11 @@
-import {createElement} from '../util.js';
+import AbstractView from './abstract.js';
 
 const createRouteFormCreateTemplate = () => (
   '<p class="trip-events__msg">Click New Event to create your first point</p>'
 );
 
-export default class EmptyList {
-  constructor() {
-    this._element = null;
-  }
-
+export default class EmptyList extends AbstractView{
   getTemplate() {
     return createRouteFormCreateTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
